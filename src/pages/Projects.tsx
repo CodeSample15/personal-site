003bbbc6603b2b84bfb2258657eaaec39a9a0049
CodeSample15 <lukecrimi15@gmail.com>
2025-08-02 23:48:54 -0400

    working on page content for projects

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,7 +1,13 @@
+import { useLocation } from "react-router-dom"
 import { ProjectCard } from "../components/ProjectCard"
+import Markdown from 'react-markdown'
+import type React from "react";
 
 export const Projects = () => {
+  const location = useLocation().pathname;
+
   return (
+    location.split('/').length == 2 ?
     <>
       <div className="d-flex m-5 p-1 gap-2 align-items-center flex-column">
         <h1 className="display-1">Projects</h1>
@@ -11,5 +17,18 @@ export const Projects = () => {
         <ProjectCard BannerSrc="Projects/Squiggly/banner.png" Name="Squiggly" Brief="An esoteric scripting language written in C++" ProjectPageUrl="/projects/Squiggly" GithubUrl="https://github.com/CodeSample15/Squiggly"/>
       </div>
     </>
+    :
+    <>
+      {getProjectPage(location.split('/')[2])}
+    </>
   )
+}
+
+function getProjectPage(location:string): React.ReactElement {
+  let markdownContent = ""
+
+  if(location === 'Squiggly') {
+  }
+
+  return <div className="m-5 p-5">{markdownContent}</div>;
 }
