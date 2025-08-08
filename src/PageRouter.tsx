7437cd15540966a84241d6c07c15eeaeb2667350
CodeSample15 <lukecrimi15@gmail.com>
2025-08-08 12:45:16 -0400

    Body padding to prevent navbar overlap

--- src/PageRouter.tsx
+++ src/PageRouter.tsx
@@ -8,18 +8,17 @@ import { Resume } from './pages/Resume';
 
 export const PageRouter = () => {
   return (
-    <Routes>
-        <Route path='/' element={ <Home/> } />
-
-        <Route path='/resume' element={ <Resume/> }/>
-
-        <Route path='/projects' element={ <Projects/> } />
-        <Route path='/projects/Squiggly' element={ <Projects page={'Squiggly'}/> } />
-        <Route path='/projects/bluetooth-led-controller' element={ <Projects/> } />
-        <Route path='/projects/keras-code-generator' element={ <Projects/> } />
-
-        <Route path='/drones' element={ <Drones/> } />
-        <Route path='*' element={ <NotFound/> } />
-    </Routes>
+    <div className="pt-3">
+      <Routes>
+          <Route path='/' element={ <Home/> } />
+          <Route path='/resume' element={ <Resume/> }/>
+          <Route path='/projects' element={ <Projects/> } />
+          <Route path='/projects/Squiggly' element={ <Projects page={'Squiggly'}/> } />
+          <Route path='/projects/bluetooth-led-controller' element={ <Projects/> } />
+          <Route path='/projects/keras-code-generator' element={ <Projects page={'Keras-Code-Generator'}/> } />
+          <Route path='/drones' element={ <Drones/> } />
+          <Route path='*' element={ <NotFound/> } />
+      </Routes>
+    </div>
   )
 }
